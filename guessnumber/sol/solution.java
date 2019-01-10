aa44d0302f4da5fb63ab55fb3ac4343f6ac4279e
Zharaskhan Aman <aman.zharaskhan@gmail.com>
2019-01-10 17:22:17 +0600

    Add optimal solution

--- guessnumber/sol/solution.java
+++ guessnumber/sol/solution.java
@@ -1,5 +1,13 @@
 class guessnumber {
     public static int guess(int n) {
-        return n;
+        int l = 1, r = n;
+        while(true) {
+            int m = (l + r) >> 1;
+            char c = grader.ask(m);
+            if(c == '=') break;
+            if(c == '<') l = m + 1;
+            if(c == '>') r = m - 1;
+        }
+        return (l + r) >> 1;
     }
 }

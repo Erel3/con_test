7818600b1a18cd5ab8158ec0f7d94acf0020e23e
Aidos <aidos.nurmash@gmail.com>
2019-01-12 00:26:29 +0600

    Fix java grader

--- xorinteractive/sol/grader.java
+++ xorinteractive/sol/grader.java
@@ -68,7 +68,7 @@ class grader {
         return pairwise_xor;
     }
 
-    public static void main(String[] args)() {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
 
